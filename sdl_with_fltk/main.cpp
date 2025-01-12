ced56d759e2009aa1975060dc4462f629df9b0e8
Gam <matyas.alfoldi@outlook.com>
2025-01-12 13:48:23 +0100

    Building with opengl

--- sdl_with_fltk/main.cpp
+++ sdl_with_fltk/main.cpp
@@ -1,17 +1,28 @@
-#include <iostream>
-
 #include <FL/Fl.H>
-#include <FL/Fl_Window.H>
-#include <FL/Fl_Box.H>
+#include <FL/Fl_Gl_Window.H>
+#include <FL/gl.h>
+#include <math.h>
+class MyWindow : public Fl_Gl_Window {
+
+public:
+    MyWindow(int x, int y, int w, int h, const char* l = 0);
+    void draw() FL_OVERRIDE;
+};
+
+MyWindow::MyWindow(int x, int y, int w, int h, const char* l)
+    : Fl_Gl_Window(x, y, w, h, l)
+{}
+
+
+void MyWindow::draw() {
+    if (!valid()) {
+    }
+
+}
 
 int main(int argc, char** argv)
 {
-    Fl_Window *window = new Fl_Window(340,180);
-    Fl_Box *box = new Fl_Box(20,40,300,100,"Hello, World!");
-    box->box(FL_UP_BOX);
-    box->labelfont(FL_BOLD+FL_ITALIC);
-    box->labelsize(36);
-    box->labeltype(FL_SHADOW_LABEL);
+    MyWindow *window = new MyWindow(0, 0, 340, 180, "Test");
     window->end();
     window->show(argc, argv);
     return Fl::run();
